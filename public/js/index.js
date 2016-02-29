55bbc33f1b73e26f759747433cff44737440e62b
Dennis Ku <deku@ucsd.edu>
2016-02-29 02:43:43 -0800

    Added tutorial to this version and jquery animations and front end changes

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+		console.log("WTF");
+        $("#soundtest").fadeIn(1500);
+        $("#equipment").delay(300).fadeIn(1500);
+        $("#environments").delay(600).fadeIn(1500);
+        $("#community").delay(900).fadeIn(1500);
+
+});
