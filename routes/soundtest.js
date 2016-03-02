549d7ffcfa32effca6542ff27ac0c21bafc7ed40
Braxton Fitts <bfitts@ucsd.edu>
2016-03-02 10:17:53 -0800

    Fixed css issue causing screw up of My Equipment and My Environment pages. Modified how the A/B versions of sound test page were handled.

--- routes/soundtest.js
+++ routes/soundtest.js
@@ -1,10 +1,12 @@
 var data = require('../data.json');
 module.exports = {
 	view_a: function (req, res) {
+        data["showAlternative"] = false;
     	res.render('soundtest', data);
 	},
     view_b: function (req, res) {
-        res.render('soundtest_b', data);
+        data["showAlternative"] = true;
+        res.render('soundtest', data);
     },
 	updateJSON: function(req,res){
 		console.log("RETURN SOUND STORAGE");
